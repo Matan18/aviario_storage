b3e983e8cb871cabadda51dc88f33f374bccaa95
Mateus Andriola <mateusandriola18@gmail.com>
2020-11-29 13:40:54 -0300

    Feature: RestHttp resources

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,48 @@
 import express from "express";
+import multer from "multer";
+import { createConnection } from "typeorm";
+import cors from 'cors';
+import uploadConfig, { tmpFolder } from "./config/upload";
+import { ProductRepository } from "./database/repository/ProductRepository";
 
 const app = express();
+app.use(cors());
+app.use(express.json())
+
+const uploadMiddleware = multer(uploadConfig);
+app.use('/files', express.static(tmpFolder));
+createConnection()
+
+app.post('/products', uploadMiddleware.single('image_url'), async (request, response) => {
+  const { name, description, quantity } = request.body;
+  const { filename } = request.file;
+  const productRepository = new ProductRepository();
+  const product = await productRepository.create({ name, description, quantity, image_url: filename });
+
+  return response.send({ product });
+})
+
+app.get('/products', async (_, response) => {
+  const productRepository = new ProductRepository();
+  const products = await productRepository.listAll();
+
+  return response.send({ products });
+})
+
+app.get('/products/search', async (request, response) => {
+  const { name } = request.query;
+  const productRepository = new ProductRepository();
+  const products = await productRepository.searchByName(name as string);
+  return response.send(products);
+})
+app.get('/products/:id', async (request, response) => {
+  const { id } = request.params;
+  const productRepository = new ProductRepository();
+  const product = await productRepository.findOne(id);
+
+  return response.send({ product });
+})
+
 
 app.listen(3333, () => {
   console.log('Sever initialized');
